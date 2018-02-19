7fdf5a9a41a56c7d50280c7aa471f6bb3b79219d
Gritsaenko Denis <belopyc@gmail.com>
2018-02-19 21:44:35 +0300

    Initial commit

---
+++ main.java
@@ -0,0 +1,13 @@
+	pckage com.java.git.test;
+
+	public class Main (
+
+		publi satatic void main(String[] args) {
+		
+			System.out.println("Hello Git");
+			for(int i = 1; i < 10; i++ ) {				
+			system.out.println(i);
+			}
+		}
+
+	)
